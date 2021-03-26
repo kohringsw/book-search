63b797db441d5a3af2202abf14600fbd4ef8b05f
shelbykohring <71856110+kohringsw@users.noreply.github.com>
2021-03-26 13:39:47 -0500

    implement Apollo Server and update auth middleware

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,5 @@
+const { User } = require('../models');
+
 const resolvers = {
     Query: {
       
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
@@ -9,10 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
-
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
