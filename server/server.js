204926b5a9ee155a88d6ae786010e4eb8ae98464
shelbykohring <71856110+kohringsw@users.noreply.github.com>
2021-04-01 14:36:15 -0500

    updating errors

--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
+if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
