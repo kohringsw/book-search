b9c8b5d98e9cba618fc17ce0e7e8e2db9765980f
shelbykohring <71856110+kohringsw@users.noreply.github.com>
2021-04-01 14:04:33 -0500

    reorder typeDefs?

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,46 +3,46 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
+type User {
+  _id: ID!
+  username: String!
+  email: String!
+  bookCount: Int!
+  savedBooks: [Book]
+}
+
+type Book {
+  bookId: ID!
+  authors: [String]
+  description: String
+  title: String!
+  image: String
+  link: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+input SaveBook {
+  authors: [String] 
+  description: String! 
+  title: String! 
+  bookId: String! 
+  image: String! 
+  link: String!
+}
+
+type Query {
     me: User
   }
 
-  input SaveBook {
-    authors: [String] 
-    description: String! 
-    title: String! 
-    bookId: String! 
-    image: String! 
-    link: String!
-}
-
-  type Mutation {
+type Mutation {
       login(email: String!, password: String!): Auth
       addUser(username: String!, email: String!, password: String!): Auth
       saveBook(input: SaveBook): User
       removeBook(bookId: String!): User
-  }
-
-  type User {
-      _id: ID!
-      username: String!
-      email: String!
-      bookCount: Int!
-      savedBooks: [type: Book]
-  }
-
-  type Book {
-      bookId: ID!
-      authors: [String]
-      description: String
-      title: String!
-      image: String
-      link: String
-  }
-
-  type Auth {
-      token: ID!
-      user: User
   }
 `;
 
